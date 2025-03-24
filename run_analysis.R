1a78928b4b36687024407430a6165cad2c507000
mxsmi <45386017+mxsmi@users.noreply.github.com>
2025-03-23 18:23:22 -0700

    Added more code for loading data
    Added more code for loading, cleaning, and merging data

--- run_analysis.R
+++ run_analysis.R
@@ -1,10 +1,15 @@
-# Load libraries
+## Load libraries
 library(dplyr)
+library(tidyr)
 
 ## Load in the test data
 activities = read.table("activity_labels.txt", col.names = c("activity_num", "activity_label"))
 test_labels = read.table("test/y_test.txt", col.names = c("activity_num"))
 subject_test = read.table("test/subject_test.txt", col.names = c("subject"))
+features = read.table("features.txt", col.names = c("feature_num", "feature_label"))
+
+test_data = read.table("test/X_test.txt")
+body_acc_x_test = read.table("test/Inertial Signals/body_acc_x_test.txt")
 
 ## clean data
 activities$activity_label = tolower(activities$activity_label)
@@ -12,3 +17,6 @@ activities$activity_label = tolower(activities$activity_label)
 ## combine data into single dataframe
 data = left_join(test_labels, activities, by = join_by(activity_num))
 data = cbind(subject_test, data)
+
+colnames(test_data) = features$feature_label
+data = cbind(data, test_data)
